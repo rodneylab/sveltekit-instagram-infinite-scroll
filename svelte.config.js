c1e470c0e12bb64c41755e3b3e171de46f191e03
Rodney Lab <codesign@rodneylab.com>
2022-03-09 07:43:02 +0000

    fix: 💫 update for SvelteKit breaking changes

--- svelte.config.js
+++ svelte.config.js
@@ -1,10 +1,7 @@
 /** @type {import('@sveltejs/kit').Config} */
 import 'dotenv/config';
 const config = {
-  kit: {
-    // hydrate the <div id="svelte"> element in src/app.html
-    target: '#svelte',
-  },
+  kit: {},
 };
 
 export default config;
