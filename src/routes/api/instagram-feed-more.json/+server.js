d9a6d50651c1a5d470e06388899f7c8f33757f80
Rodney Lab <codesign@rodneylab.com>
2022-09-13 10:23:07 +0100

    style: 🛁  linting

--- src/routes/api/instagram-feed-more.json/+server.js
+++ src/routes/api/instagram-feed-more.json/+server.js
@@ -1,19 +1,15 @@
+import { error } from '@sveltejs/kit';
+
 /** @type {import('./$types').RequestHandler} */
 export async function POST({ request }) {
 	try {
 		const { next } = await request.json();
-		const response = await fetch(next, {
-			method: 'GET',
-		});
-
+		const response = await fetch(next);
 		const data = await response.json();
 
 		return new Response(JSON.stringify(data));
 	} catch (err) {
 		console.log('Error: ', err);
-		return {
-			status: 500,
-			error: 'Error retrieving data in /api.instagram-feed-more.json',
-		};
+		throw error(500, 'Error retrieving data in /api.instagram-feed-more.json');
 	}
 }
